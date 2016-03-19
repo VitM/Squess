63b77fa60f747177416ed8cec4a22c04ea6cb0ad
VitM <vit.mistry@gmail.com>
2016-03-19 14:28:27 +0000

    Initial work to get the project working with CLion

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    cout << "Press any key to continue." << endl;
+
+    int x  = 0;
+    cin >> x;
+
+    return 0;
+}
